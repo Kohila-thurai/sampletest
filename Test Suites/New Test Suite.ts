f51e71de5158abf358176cf44ae53ff2a5f3d53a
kohilat <kohilat@LapWinX-DAC59.arackal.co.in>
2023-09-21 18:03:22 +0530

    commit 2

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>6cb0a76f-9cfd-4731-ad2d-a1d4079df229</testSuiteGuid>
+</TestSuiteEntity>
